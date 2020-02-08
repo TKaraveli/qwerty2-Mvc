2473bd1671f9cd1118b849561fae930b5eb1a19a
TKaraveli <tayfunkaraveli@hotmail.com>
2020-02-08 03:01:56 +0300

    Entities class library oluşturuldu.

---
+++ Qwerty2.Entities/AboutPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Qwerty2.Entities
+{
+    public class AboutPage
+    {
+        public int Id { get; set; }
+
+        public string TitleName { get; set; }
+
+        public string AboutDescription { get; set; }
+
+    }
+}
---
+++ Qwerty2.Entities/ContactPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Qwerty2.Entities
+{
+    public class ContactPage
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string MyProperty { get; set; }
+
+        public string EmailAdress { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string LinkedInProfile { get; set; }
+
+        public string GitHubProfile { get; set; }
+
+    }
+}
